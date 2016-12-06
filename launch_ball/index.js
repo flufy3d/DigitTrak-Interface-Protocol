59827a6732143ebf0d5ad4935c8aae5c732e4151
flufy3d <flufy3d@gmail.com>
2016-12-06 12:37:30 +0800

    bind data model

--- launch_ball/index.js
+++ launch_ball/index.js
@@ -6,19 +6,46 @@ jQuery.fn.pickify = function() {
 		$input = $picker.children('input.change');
 		$board = $picker.children('.board');
 		$choice = $board.children();
-		$rainbow = $picker.children('.rainbow');
+
+		$pitch = $picker.children('#pitch');
+		$backspin = $picker.children('#backspin');
+		$sidespin = $picker.children('#sidespin');
+		$confidence = $picker.children('#confidence');
+
 
 		// making things happen
-		$rainbow.slider({
-      	value: 100,
-      	min: 0,
-      	max: 360,
-			slide: function(event, ui) {console.log('slide:'+ui.value)},
-			stop: function() {}
-    	})
+
     
-		var xy = $picker.attr('data-xy').split(',');		
-		$picker.data('xy', {x: Number(xy[0]), y: Number(xy[1])})
+		var xy = $picker.attr('data-xy').split(',');	
+		change_xy(Number(xy[0]),Number(xy[1]));
+
+
+		function init_comp(obj ,min ,max ,def ,tag )
+		{
+
+			$picker.data(tag, def);
+			obj.slider({
+	      	value: $picker.data(tag),
+	      	min: min,
+	      	max: max,
+	      		create: function(){obj.children('label').text(tag + ': ' + $picker.data(tag));},
+				slide: function(event, ui) {
+					obj.children('label').text(tag + ': ' + ui.value);
+					$picker.data(tag, ui.value);
+				},
+				stop: function() {}
+	    	})
+
+
+		}
+
+		init_comp($pitch,0,90,18,"pitch");
+		init_comp($backspin,0,9000,7000,"backspin");
+		init_comp($sidespin,-1000,1000,418,"sidespin");
+		init_comp($confidence,0,1.0,1,"confidence");
+
+
+	
 
 		$choice.draggable({
 			containment: '.board',
@@ -40,10 +67,30 @@ jQuery.fn.pickify = function() {
 
 		function change_xy(x,y)
 		{
-			$picker.data('xy', {x:x, y:y});
 			$choice.css({'left': x, 'top': y});
-			$input.val('D: '+ x + ' V: ' + y);
+
+			var width = 180;
+			var height = 180;
+			var start = {x:width/2,y:height};
+			var end = {x:x, y:y};
+
+			$picker.data('xy', end);
+
+			var vec = {x:end.x-start.x,y:end.y-start.y}
 			
+			var vec_length = Math.sqrt(vec.x*vec.x + vec.y*vec.y)
+
+			var all_length = Math.sqrt(width*width + height*height)
+
+			var v = vec_length/all_length * 120.0
+
+			var d = - Math.acos(vec.x/vec_length)/Math.PI * 180 + 90
+
+			$input.val('D: '+ d.toFixed(2) + ' V: ' + v.toFixed(2));
+
+			$picker.data('yaw', d);
+			$picker.data('velocity', v);
+
 		}
 
 	
